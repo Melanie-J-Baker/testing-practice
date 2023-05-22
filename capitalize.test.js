0a145abaa29c008a4bd90575877889538ac0f436
Melanie-J-Baker <baker_mel@hotmail.com>
2023-05-22 11:29:30 +0100

    Change it to test

--- capitalize.test.js
+++ capitalize.test.js
@@ -2,18 +2,18 @@
 
 const capitalize = require('./capitalize');
 
-it('first letter is capitalized', () => {
+test('first letter is capitalized', () => {
     expect(capitalize("mulberry")).toBe("Mulberry");
 })
 
-it('converts string in capitals', () => {
+test('converts string in capitals', () => {
     expect(capitalize("MULBERRY")).toBe("Mulberry");
 })
 
-it('works for a sentence', () => {
+test('works for a sentence', () => {
     expect(capitalize("what is the meaning of life")).toBe("What is the meaning of life");
 })
 
-it('ignores punctuation', () => {
+test('ignores punctuation', () => {
     expect(capitalize("what's the meaning of life?")).toBe("What's the meaning of life?");
 })
