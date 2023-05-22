346dc98a8f763c8b56a4cddd08673a688cecd718
Melanie-J-Baker <baker_mel@hotmail.com>
2023-05-22 12:19:22 +0100

    Write calculator tests and create calculator object

--- calculator.js
+++ calculator.js
@@ -1,7 +1,40 @@
-// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+// A calculator object that contains functions for the basic operations: 
+// add, subtract, divide, and multiply. 
+// Each of these functions should take two numbers and return the correct calculation.
 
-const calculator = () => {
+const valid = (num1, num2) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    } else {
+        return true;
+    }
+};
 
+const calculator = {
+    add(num1, num2) {
+        if (valid(num1, num2)) {
+            return num1 + num2;
+        }
+        return 'One/both argument(s) not a number';
+    },
+    subtract(num1, num2) {
+        if (valid(num1, num2)) {
+            return num1 - num2;
+        }
+        return 'One/both argument(s) not a number';
+    },
+    divide(num1, num2) {
+        if (valid(num1, num2)) {
+            return num1 / num2;
+        }
+        return 'One/both argument(s) not a number';
+    },
+    multiply(num1, num2) {
+        if (valid(num1, num2)) {
+            return num1 * num2;
+        }
+        return 'One/both argument(s) not a number';
+    }
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,41 @@
-// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+// A calculator object that contains functions for the basic operations: 
+// add, subtract, divide, and multiply. 
+// Each of these functions should take two numbers and return the correct calculation.
 
 const calculator = require('./calculator');
 
-it('works', () => {
-    expect(1).toBe(1)
+test('if input not a number', () => {
+    expect(calculator.add("A", "?")).toBe("One/both argument(s) not a number");
+})
+
+test('if input not a number', () => {
+    expect(calculator.subtract("A", "?")).toBe("One/both argument(s) not a number");
+})
+
+test('if input not a number', () => {
+    expect(calculator.divide("A", "?")).toBe("One/both argument(s) not a number");
+})
+
+test('if input not a number', () => {
+    expect(calculator.multiply("A", "?")).toBe("One/both argument(s) not a number");
+})
+
+test('adds two integers', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+})
+
+test('subtracts two integers', () => {
+    expect(calculator.subtract(10, 5)).toBe(5);
+})
+
+test('divides two integers', () => {
+    expect(calculator.divide(10, 5)).toBe(2);
+})
+
+test('multiplies two integers', () => {
+    expect(calculator.multiply(10, 5)).toBe(50);
+})
+
+test('works with floating points', () => {
+    expect(calculator.add(1.5, 1.5)).toBe(3);
 })
